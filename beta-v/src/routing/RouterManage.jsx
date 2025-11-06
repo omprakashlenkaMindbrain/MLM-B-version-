48f94d57d6b93097d3ded99fbbe6bce9d3dfad9c
Singh123-spec <subhrajit.mindbrain@gmail.com>
2025-11-06 12:57:01 +0530

    profilepage updated

--- beta-v/src/routing/RouterManage.jsx
+++ beta-v/src/routing/RouterManage.jsx
@@ -1,4 +1,9 @@
-import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import { AuthProvider } from "../context/AuthContext";
@@ -8,45 +13,56 @@ import SignupPage from "../pages/Authentication/Signup";
 import KYCPage from "../pages/Details/KYC";
 import Plans from "../pages/Details/Plans";
 import Home from "../pages/Home/Home";
+import ProfilePage from "../pages/profile/ProfilePage";
 
 function Layout() {
-    const location = useLocation();
-    const hideNavbarPath = ['/login', '/register'];
+  const location = useLocation();
+  const hideNavbarPath = ["/login", "/register"];
 
-    return (
-        <>
-            {!hideNavbarPath.includes(location.pathname) && <Navbar />}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<SignupPage />} />
-                <Route
-                    path="/kyc"
-                    element={
-                        <ProtectedRoute>
-                            <KYCPage />
-                        </ProtectedRoute>
-                    } />
-                <Route
-                    path="/plans"
-                    element={
-                        <ProtectedRoute>
-                            <Plans />
-                        </ProtectedRoute>
-                    } />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      {!hideNavbarPath.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignupPage />} />
+        <Route
+          path="/kyc"
+          element={
+            <ProtectedRoute>
+              <KYCPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/plans"
+          element={
+            <ProtectedRoute>
+              <Plans />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 function RouterManage() {
-    return (
-        <Router>
-            <AuthProvider>
-                <Layout />
-            </AuthProvider>
-        </Router>
-    );
+  return (
+    <Router>
+      <AuthProvider>
+        <Layout />
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default RouterManage;
